c9be8bce47025b9980e1ab8685a3cf7a008ec301
Rafael Neves <rafaelneves.130@gmail.com>
2025-03-09 00:01:58 -0300

    feat: implements people routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\StarWarsPeopleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
+});
+
+Route::prefix('api/v1')->group(function () {
+  Route::get('/people/search', [StarWarsPeopleController::class, 'search']);
+  Route::get('/people/{id}', [StarWarsPeopleController::class, 'show']);
 });
