00fd64debc7d8be4526b462677f580db4f71ce70
Rafael Neves <rafaelneves.130@gmail.com>
2025-03-10 16:13:38 -0300

    feat: implements website route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SearchPage from './pages/SearchPage';
+import Layout from './components/Layout';
+import PersonPage from './pages/PersonPage';
+import FilmPage from './pages/FilmPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<SearchPage />} />
+          <Route path='/people/:id' element={<PersonPage />} />
+          <Route path='/films/:id' element={<FilmPage />} />
+        </Routes>
+      </Layout >
+    </BrowserRouter >
   );
 }
 
