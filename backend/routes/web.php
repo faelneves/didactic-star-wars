b0d700395daf4c9cda7d1f0aca4b7c21cba56dbb
Rafael Neves <rafaelneves.130@gmail.com>
2025-03-10 16:04:15 -0300

    feat: implements films routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\StarWarsPeopleController;
+use App\Http\Controllers\StarWarsFilmsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,4 +11,7 @@ Route::get('/', function () {
 Route::prefix('api/v1')->group(function () {
   Route::get('/people/search', [StarWarsPeopleController::class, 'search']);
   Route::get('/people/{id}', [StarWarsPeopleController::class, 'show']);
+
+  Route::get('/films/search', [StarWarsFilmsController::class, 'search']);
+  Route::get('/films/{id}', [StarWarsFilmsController::class, 'show']);
 });
